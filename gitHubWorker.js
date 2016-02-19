e96e4689379aea9999cd2cc1cccd51ce3768d0d0
Daniel Kim <dkim2707@gmail.com>
2016-02-18 16:57:46 -0800

    Implement getIssue worker function returns a list of 100 issues

--- gitHubWorker.js
+++ gitHubWorker.js
@@ -1,33 +1,39 @@
-
+// Require the request module from node
 var request = require('request');
 
+module.exports = {};
 
-var getGitHubIssue = function (user, callback) {
+//Create a get request for issues
+module.exports.getIssues = function (callback) {
  var options = {
-   url: 'https://api.github.com/users/'+user,
-   headers: { 'User-Agent': 'request' },
+   url: 'https://api.github.com/search/issues?q=is%3Aissue+is%3Aopen+label%3A%22good+first+bug%22&per_page=100',
+   headers: { 'User-Agent': '' },
    json: true  // will JSON.parse(body) for us
  };
-
+ 
  request.get(options, function (err, res, body) {
    if (err) { 
      callback(err, null);
-   } else if (body.message) {
-     callback(new Error('Failed to get GitHub profile: ' + body.message), null);
    } else {
      callback(null, body);
    }
  });
 };
 
-
-getGitHubIssue('danielsunkim', function (err, body) {
+/*
+module.exports.getIssues(function (err, body) {
+  if (err) {
   console.log('err', err);
+    
+  } else {
   console.log('body', body);
+    
+  }
 });
+*/
 
 /*
 // Grab the github endpoints
-
 curl https://api.github.com
 */
+
