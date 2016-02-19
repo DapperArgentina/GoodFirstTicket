ed4949d199fff34faaed71781c187d7ed2cd7f95
Vince Cipriani <vince.cipriani@gmail.com>
2016-02-18 23:40:52 -0800

    update Search component to actually call the API w/ search term

--- client/components/TicketSearch.js
+++ client/components/TicketSearch.js
@@ -6,13 +6,27 @@ class TicketSearch extends React.Component {
     super(props);
 
     this.state = {
-      searchText: props.initialSearch
+      searchText: null
     };
+    
+    this.searchHandler = this.searchHandler.bind(this);
   }
 
+  searchHandler(e) {
+    //If it is called by someone pressing enter, we run the searchHandler provided to use
+    if (e.charCode === 13 || e.keyCode === 13) {
+      this.props.searchHandler(e.target.value);
+    }
+    
+    //In all cases we update our component state
+    this.setState({
+      searchText: e.target.value
+    });
+  }
   render () {
     return <div className="col-md-10 col-md-offset-1">
-              <input className="form-control" type="text" value={this.state.searchText} placeholder="Search here..." />
+              <input className="form-control" type="text" value={this.state.searchText} 
+                placeholder="Search here..." onChange={this.searchHandler} onKeyPress={this.searchHandler} />
            </div>;
   }
 }
