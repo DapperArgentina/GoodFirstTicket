113f85387f096084cc5fcf0959b0c9c0c0f7d28d
Megan Ring <meganbethring@gmail.com>
2016-02-22 15:19:53 -0800

    Initialize a repo list component and corresponding repoEntry component; create getRepos function in gitHubWorker; delete language placeholder in ticket entry component

---
+++ client/components/RepoList.js
@@ -0,0 +1,53 @@
+const React = require('react');
+const RepoSearch = require('./RepoSearch');
+const RepoEntry = require('./RepoEntry');
+const $ = require('jquery');
+const GH = require('../../gitHubWorker');
+
+class RepoList extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      repos: []
+    };
+    
+    this.getRepos = this.getRepos.bind(this);
+    
+    this.getRepos();
+  }
+  
+  getRepos(searchTerm, language){
+    //Fetch repos;
+    //refactor to exclude 'self/this' with es6 syntax?
+    var self = this;
+    GH.getRepos(function(data) {
+      console.log(data);
+      self.setState({
+        repos: data
+      });
+    }, console.log, searchTerm, language);
+  }
+  
+  render () {
+    
+    //for really clean scrolling, we could do something like below to calculate the max height and then set the max height css 
+    // var maxHeight = $(window).height() - $('.navbar').outerHeight() - margin * 2;
+    
+    return (
+    <div >
+      <RepoSearch searchHandler={this.getRepos} />
+      <h4>Repositories</h4>
+      <div className="main-repo-view">
+        {this.state.tickets.map ((repo, index) => 
+          <RepoEntry data={repo} key={index} />
+        )}
+      </div>
+    </div>
+    );  
+  }
+  
+}
+
+module.exports = RepoList;
