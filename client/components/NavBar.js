65101c17e79995fd9a058842ae7c2539d59e9d95
Megan Ring <meganbethring@gmail.com>
2016-02-22 14:22:52 -0800

    start react-router, get default route to successfully render

--- client/components/NavBar.js
+++ client/components/NavBar.js
@@ -1,5 +1,8 @@
 const NavEntry = require('./NavEntry');
 const React = require('react');
+const Router = require('react-router').Router;
+const Route = require('react-router').Route;
+const Link = require('react-router').Link;
 
 
 const NavBar = (props) => (
--- client/init.js
+++ client/init.js
@@ -2,11 +2,24 @@
 //Code that starts app goes here
 const React = require('react');
 const ReactDOM = require('react-dom');
+import { Router, Route, Link, IndexRoute, hashHistory } from 'react-router'
+const RepoProfile = require('./components/RepoProfile'); 
+const TicketList = require('./components/TicketList'); 
 
 const App = require('./components/app');
 const data = require('./dummyData');
 
-ReactDOM.render(<App tickets={data.items}/> , document.getElementById('app'));
+ReactDOM.render((
+  <Router history={hashHistory}>
+    <Route path='/' component={App}>
+      <IndexRoute component={TicketList} />
+      <Route path='repoProfile' component={RepoProfile} />
+      {// <Route path='repos' component={RepoList} />
+      // <Route path='resources' component={ResourceList} />
+      }
+    </Route>
+  </Router>
+), document.getElementById('app'))
 
 //Makes dropdowns work in materialize. Should run after app has loaded.
 $(document).ready(function() {
