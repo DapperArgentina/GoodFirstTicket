6e75b9b67730513e12f817914db32931d8fa9e18
Megan Ring <meganbethring@gmail.com>
2016-02-22 15:58:35 -0800

    Connect repo list link to repo list page; repo profile link from repo list view is still a dead link

--- client/init.js
+++ client/init.js
@@ -3,8 +3,10 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
 import { Router, Route, Link, IndexRoute, hashHistory } from 'react-router'
-const RepoProfile = require('./components/RepoProfile'); 
 const TicketList = require('./components/TicketList'); 
+const RepoList = require('./components/RepoList'); 
+const RepoProfile = require('./components/RepoProfile'); 
+
 
 const App = require('./components/app');
 const data = require('./dummyData');
@@ -13,8 +15,10 @@ ReactDOM.render((
   <Router history={hashHistory}>
     <Route path='/' component={App}>
       <IndexRoute component={TicketList} />
+      <Route path='repos' component={RepoList} />
       <Route path='repoProfile' component={RepoProfile} />
-      {// <Route path='repos' component={RepoList} />
+      {
+      // <Route path='repoProfile' component={RepoProfile} />
       // <Route path='resources' component={ResourceList} />
       }
     </Route>
