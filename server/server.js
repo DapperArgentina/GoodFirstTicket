bad6be00da90c9dbf7768cfaf17a91304d9ac966
Vince Cipriani <vince.cipriani@gmail.com>
2016-02-22 11:38:01 -0800

    add cors headers

--- server/server.js
+++ server/server.js
@@ -6,12 +6,13 @@ var app = express();
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/../client'));
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
 var port = process.env.PORT || 3000;
-console.log(`server running on port ${port}`);
-// start listening to requests on port 3000
-app.listen(port);
-
 
 app.route('/api')
   .get(function(req, res){
@@ -27,7 +28,7 @@ app.route('/api/issues')
     })
     .then ( function (results) {
       res.send(results);
-    })
+    });
   });
 
 app.route('/api/repos')
@@ -41,6 +42,9 @@ app.route('/api/repos')
     }) 
   });
 
+console.log(`server running on port ${port}`);
+// start listening to requests on port 3000
+app.listen(port);
 
 // export our app for testing and flexibility, required by index.js
 module.exports = app;
