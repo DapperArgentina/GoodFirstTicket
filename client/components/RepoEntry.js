30c3091f40dfe8bf2ddfb51d8e18caa5a8e85aec
Colin Whitmarsh <colin.whitmarsh@gmail.com>
2016-02-23 15:02:42 -0800

    front-end: bold text on RepoCard

--- client/components/RepoEntry.js
+++ client/components/RepoEntry.js
@@ -15,15 +15,15 @@ const RepoEntry = (props) => (
                 <p className="left-align grey-text lighten-2 col s12">{props.data.description}</p>
               </div>
               <div className="row">
-                <p className="left-align col s3">Updated <TimeAgo date={props.data.updated_at} /></p>
-                <p className="center col s3"><span className="octicon octicon-issue-opened"></span> Beginner tickets {props.data.beginner_tickets}</p>
-                <p className="center col s3"><span className="octicon octicon-git-branch"></span> Forks {props.data.forks}</p>
+                <strong className="left-align col s3"><span className="octicon octicon-history"></span> Updated <TimeAgo date={props.data.updated_at} /></strong>
+                <strong className="center col s3"><span className="octicon octicon-issue-opened"></span> Beginner tickets {props.data.beginner_tickets}</strong>
+                <strong className="center col s3"><span className="octicon octicon-git-branch"></span> Forks {props.data.forks}</strong>
                 <div className="right-align col s3 mega-octicon octicon-thumbsup"></div>
               </div>
               <div className="row">
-                <p className="left-align col s3 cyan-text lighten-2">{props.data.org_name}</p>
-                <p className="center col s3" ><a className="cyan-text lighten-2" href={props.data.html_url} target="_blank">Repo on GitHub</a></p>
-                <p className="center col s3">{props.data.language}</p>
+                <strong className="left-align col s3 cyan-text lighten-2">{props.data.org_name}</strong>
+                <strong className="center col s3" ><a className="cyan-text lighten-2" href={props.data.html_url} target="_blank">Repo on GitHub</a></strong>
+                <strong className="center col s3">{props.data.language}</strong>
                 <div className="right-align col s3 mega-octicon octicon-thumbsdown"></div>
               </div>
           </div>
