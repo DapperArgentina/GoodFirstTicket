2e8cf74e2ec9d5c41da4c9cde8b70b45cdae26fe
Vince Cipriani <vince.cipriani@gmail.com>
2016-02-23 15:06:00 -0800

    fix array comparisons, comparing to empty array does not work

--- server/models/repos.js
+++ server/models/repos.js
@@ -12,7 +12,7 @@ Repos.prototype.getRepos = function () {
   var self = this;
   var hoursSinceLastFetch = new DateDiff(new Date(), this._lastUpdateDate).hours();
   
-  if (this._repos === [] ||
+  if (this._repos.length === 0 ||
    hoursSinceLastFetch > 1) {
     return db.raw(`select * from repos`)
             .then((results) => {
