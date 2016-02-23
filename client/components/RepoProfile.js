3b3a51b62c56de4ff2cfb0370725b9606f374430
Daniel Kim <dkim2707@gmail.com>
2016-02-23 12:00:16 -0800

    Add static data to repoProfile

--- client/components/RepoProfile.js
+++ client/components/RepoProfile.js
@@ -1,5 +1,28 @@
 const React = require('react');
 
+const data = {
+  "internal_id":1,
+  "id":1357796,
+  "name":"emscripten",
+  "org_name":"kripken",
+  "html_url":"https://github.com/kripken/emscripten",
+  "language":"C",
+  "beginner_tickets":35,
+  "description":"Emscripten: An LLVM-to-JavaScript Compiler",
+  "stargazers_count":9723,
+  "watchers_count":9723,
+  "has_wiki":1,
+  "has_pages":0,
+  "open_issues":740,
+  "forks":1177,
+  "created_at":"2011-02-12T05:23:30.000Z",
+  "updated_at":"2016-02-22T04:27:46.000Z",
+  "pushed_at":"2016-02-20T19:46:56.000Z",
+  "data_refreshed_at":"2016-02-22T05:14:34.000Z",
+  "record_inserted_at":"2016-02-22T05:14:34.000Z",
+  "etag":"\"5621117469930ec9afd5538762cb0514\""
+};
+
 const RepoProfile = (props) => (
   <div className="repo-profile">
     <h1 classname="jumbotron" href={props.repo.html_url}>{props.repo.name}</h1>
