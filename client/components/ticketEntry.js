9638c8ec52312094ff42758f7fd83c2e2af406f7
Megan Ring <meganbethring@gmail.com>
2016-02-24 14:17:36 -0800

    Add labels to issues cards

--- client/components/ticketEntry.js
+++ client/components/ticketEntry.js
@@ -4,22 +4,34 @@ const Link = require('react-router').Link;
 
 const TicketEntry = (props) => (
   <div className="row">
-      <div className="col s12 m10">
-        <div className="card white">
-          <div className="card-content black-text" >
-            <span className="card-title"><a className="cyan-text lighten-2" href={props.data.html_url} target="_blank">{props.data.title}</a></span>
-              <div className="row">
-                <p className="left-align col s6"><span className="octicon octicon-calendar"></span> Created <TimeAgo date={props.data.created_at} /></p>
-              </div>
-              <div className="row">
-                <p className="left-align col s6">Repo: <Link className="cyan-text lighten-2" to={`/repoProfile/${props.data.repo_id}`}>{props.data.repo_name}
-                </Link></p>
-                <p className="right-align col s6">{props.data.language}</p>
-              </div>
+    <div className="col s12 m10">
+      <div className="card white">
+        <div className="card-content black-text" >
+          <span className="card-title"><a className="cyan-text lighten-2" href={props.data.html_url} target="_blank">{props.data.title}</a></span>
+          <div className="row">
+            <div className="col sm 12">  
+              {props.data.labels.map(function(label, index) {
+                return (
+                  <div className="chip" style={{'backgroundColor': '#' + label.color}} key={index}>
+                    {label.name}
+                  </div>
+                )
+              }
+            )}
+            </div>
+          </div>
+          <div className="row">
+            <p className="left-align col s6"><span className="octicon octicon-calendar"></span> Created <TimeAgo date={props.data.created_at} /></p>
+          </div>
+          <div className="row">
+            <p className="left-align col s6">Repo: <Link className="cyan-text lighten-2" to={`/repoProfile/${props.data.repo_id}`}>{props.data.repo_name}
+            </Link></p>
+            <p className="right-align col s6">{props.data.language}</p>
           </div>
         </div>
       </div>
     </div>
+  </div>
 );
 
 module.exports = TicketEntry;
