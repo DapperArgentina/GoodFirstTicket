99eaef95d9551ce44d160c31fb8eda39a52f9a2a
Vince Cipriani <vince.cipriani@gmail.com>
2016-02-24 07:57:23 -0800

    improve QueueManager to be able to take into account cases where the request does not count against us

--- server/data-processor/queueManager.js
+++ server/data-processor/queueManager.js
@@ -1,12 +1,12 @@
 const DateDiff = require('date-diff');
 
-var QueueManager = function(reqPerMin) {
+var QueueManager = function(reqPerMin, statusCodeToNotCount) {
   this.queue = [];
   this.reqPerMin = reqPerMin;
   this.requestsRemaining = reqPerMin;
   this.sessionStartTime = new Date();
   this.secondsBeforeRequestReset = 60;
-  
+  this.statusCodeToNotCount = statusCodeToNotCount;
   this.dequeue();
 };
 
@@ -36,10 +36,17 @@ QueueManager.prototype.dequeue = function() {
     var item = this.queue.shift();  
     item.fn.apply(null, item.params)
       .then((result) => item.resolve(result))
-      .catch((err) => item.reject(err));
-    self.dequeue();
+      .catch((err) => {
+        if(err.statusCode === self.statusCodeToNotCount) {
+          self.requestsRemaining++;
+        }
+        item.reject(err);
+      });
+    //We delay our next dequeue slightly so we have time to get a reponse from previous request
+    //This allows us to take advantage of the case when we have statusCodeToNotCount and some requests
+    //end up not counting against us
+    setTimeout(this.dequeue.bind(this), 100);
   } else if (this.requestsRemaining > 1) {
-    console.log('Nothing in queue.  Waiting 1s.');
     setTimeout(this.dequeue.bind(this), 1000);
   } else {
     //We are out of requests.  Schedule a wait until the reset.  We add 100ms to make sure
