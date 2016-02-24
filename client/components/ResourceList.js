087311b30cc8489fb5d20fd94d1a55e40143862e
Daniel Kim <dkim2707@gmail.com>
2016-02-23 16:46:50 -0800

    Add a ResourceList view

---
+++ client/components/ResourceList.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const ResourceList = (props) => (
+    <div>
+      <div>
+        <div>
+          <div>
+            <span>adf</span>
+            <div>
+              <p>adf</p>
+              <p>adf</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+);
+
+module.exports = ResourceList;
