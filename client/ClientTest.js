f04a62221aea6f30f957864a44593840b2ad5ff4
Daniel Kim <dkim2707@gmail.com>
2016-02-29 11:24:21 -0800

    Add skeleton for the DataTest, ServerTest, and ClientTest. Update

---
+++ client/ClientTest.js
@@ -0,0 +1,40 @@
+var expect = require('chai').expect;
+var assert = require('chai').assert;
+var should = require('chai').should();
+
+describe('Client', function() {
+
+  it('should pass the test', function (done) {
+    done();
+  });
+
+  it('should pass the test', function (done) {
+    done();
+  });
+
+  it('should pass the test', function (done) {
+    done();
+  });
+
+  it('should pass the test', function (done) {
+    done();
+  });
+
+  it('should pass the test', function (done) {
+    done();
+  });
+
+  it('should pass the test', function (done) {
+    done();
+  });
+
+  it('should pass the test', function (done) {
+    done();
+  });
+
+  it('should pass the test', function (done) {
+    done();
+  });
+
+
+});
