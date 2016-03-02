9eaf3c8caf0762fe3947b52dc78d7db97e51bdb8
Joe Miller <joemillervi@gmail.com>
2016-03-02 14:29:22 -0800

    add ternary when redirecting not signed in user

--- client/components/NavEntry.js
+++ client/components/NavEntry.js
@@ -5,7 +5,7 @@ const Link = require('react-router').Link;
 
 const NavEntry = (props) => (
   <li className='indigo accent-2'>
-    <Link className='white-text' to={this.props.loggedIn ? props.data.url : '/login'}>{props.data.name}</Link>
+    <Link className='white-text' to={props.loggedIn && (props.data.name !== 'Bounties') ? props.data.url : '/login'}>{props.data.name}</Link>
   </li>
 );
 
