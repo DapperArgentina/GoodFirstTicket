a94706ca8ca23d96ae69bb5fc88c4b2cce503a72
franklinshieh <franklinshieh@gmail.com>
2016-03-02 16:05:37 -0800

    Adds in add bounty form with stripe

---
+++ client/components/BountyForm.js
@@ -0,0 +1,67 @@
+const React = require('react');
+
+class BountyForm extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      card: {
+        number: '',
+        cvc: '',
+        exp_month: '',
+        exp_year: ''
+      }
+    };
+  }
+
+  componentDidMount() {
+    Stripe.setPublishableKey('pk_test_4SrTTNmWSmtYCG2BxAYseTE9'); // set your test public key
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    Stripe.createToken(this.state.card, function (status, response) {
+      console.log( status, response );
+    });
+  }
+
+  handleChange(e) {
+    let card = this.state.card;
+    card[e.target.name] = e.target.value;
+    this.setState(card);
+  }
+
+  render() {
+    return (
+      <div className="bountyFormDiv">
+        <h1>Post a Bounty</h1>
+        <form onSubmit={ this.handleSubmit.bind(this) }>
+          <div class="form-row">
+            <label>
+            <span>Card Number</span>
+            <input maxLength="20" name="number" required onChange={this.handleChange.bind(this)} />
+            </label>
+          </div>
+          <div class="form-row">
+            <label>
+              <span>CVC</span>
+              <input maxLength="4" name="cvc" required onChange={this.handleChange.bind(this)} />
+            </label>
+          </div>
+          <div class="form-row">
+            <label>
+              <span>Expiration (MM/YYYY)</span>
+            <input maxLength="2" name="exp_month" required onChange={this.handleChange.bind(this)} />
+            </label>
+            <span> / </span>
+            <input maxLength="4" name="exp_year" required onChange={this.handleChange.bind(this)} />
+          </div>
+          <button type="submit">Enter</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+module.exports = BountyForm;
